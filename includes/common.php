a6add4c56d23c0a8202d865933f4112bbfa6275f
VemulaPallavi <118461080+VemulaPallavi@users.noreply.github.com>
2023-06-10 00:15:52 +0530

    initt

--- includes/common.php
+++ includes/common.php
@@ -1,6 +1,6 @@
 <!--establish the connection to database, and start the session-->
 <?php
-$con = mysqli_connect("localhost:3322", "root", "", "store");
+$con = mysqli_connect("localhost:3322", "root", "","store");
 if(session_status() == PHP_SESSION_NONE){
     session_start();
 }
--- onlinepayment.php
+++ onlinepayment.php
@@ -4,12 +4,11 @@ require("includes/common.php");
 if (!isset($_SESSION['email'])) {
     header('location: index.php');
 }
-$user_id = $_SESSION['user_id'];
-$item_ids_string = $_GET['itemsid'];
+
 
 //We will change the status of the items purchased by the user to 'Confirmed'
-$query = "UPDATE user_item SET status=2 WHERE user_id=" . $user_id . " AND item_id IN (" . $item_ids_string . ") and status= 1 ";
-mysqli_query($con, $query);
+
+
 ?>
 
 <!DOCTYPE html>
@@ -30,12 +29,9 @@ mysqli_query($con, $query);
                     <img src="img/thanks.png" style="float: left;">
                 </div>
                 <div class="jumbotron">
-                      <h3 align="center">YAY!! Your order is confirmed. Thank you for shopping with us.</h3><hr>
+                      <h3 align="center">YAY!! Your Payment is successfully done. Thank you for shopping with us.</h3><hr>
 <p align="center">Click <a href="products.php">here</a> to purchase any other item.</p>
-</br></br><h2 align="center"><bold>PAYMENT MODE</bold></h2></br>
-        <input type="radio" id="payment" size="30" >Online Payment
-<a  href="payment.html" class="btn btn-danger btn-lg active">PAY NOW</a><br><br>
-<input type="radio" id="payment" size="30">Cash On Delivery
+
                 </div>
             </div>
 
